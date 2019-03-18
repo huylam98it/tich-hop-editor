4c242ab7755966af4bf00e783dbb12a2981f841c
huylam98it <huylam98it@gmail.com>
2019-03-19 03:36:39 +0700

    da add project

---
+++ tichhoptexteditor/src/main/java/com/huylam98it/tichhoptexteditor/TichhoptexteditorApplication.java
@@ -0,0 +1,13 @@
+package com.huylam98it.tichhoptexteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TichhoptexteditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TichhoptexteditorApplication.class, args);
+	}
+
+}
